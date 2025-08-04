4524b46b2ee681b9bd269863ea4b9bcc7ea9d07a
dexter36372 <dexterkenny2008@gmail.com>
2025-08-04 10:42:54 +0100

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,18 @@
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAFpMHaHabCvlmO5BvbgKIRsEBt4BtJ9K0",
+  authDomain: "feva-hotel.firebaseapp.com",
+  projectId: "feva-hotel",
+  storageBucket: "feva-hotel.firebasestorage.app",
+  messagingSenderId: "805719023406",
+  appId: "1:805719023406:web:283442e30ede417f9b24bf",
+  measurementId: "G-QQXQED1XWL"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
