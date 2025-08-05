bf8ef69b65f17b1224a3ef9a0dda8c4a3ce0553e
dexter36372 <dexterkenny2008@gmail.com>
2025-08-05 23:22:17 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// Replace `yourusername` and `reponame` with your actual GitHub details
 export default defineConfig({
-  base: '/feva-hotel/',
   plugins: [react()],
-})
+  server: {
+    allowedHosts: [
+      "a86da958-68ae-4eeb-a7b8-662ff291c560-00-3vz612z9wxxrg.kirk.replit.dev"
+    ]
+  }
+});
 
