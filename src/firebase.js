24ed9357564bc533d5c1d13aaedf465cb33240aa
dexter36372 <dexterkenny2008@gmail.com>
2025-08-06 11:53:33 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,18 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getDatabase, ref, get, child } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAFpMHaHabCvlmO5BvbgKIRsEBt4BtJ9K0",
   authDomain: "feva-hotel.firebaseapp.com",
+  databaseURL: "https://feva-hotels-default-rtdb.firebaseio.com",
   projectId: "feva-hotel",
-  storageBucket: "feva-hotel.firebasestorage.app",
+  storageBucket: "feva-hotel.appspot.com",
   messagingSenderId: "805719023406",
   appId: "1:805719023406:web:283442e30ede417f9b24bf",
   measurementId: "G-QQXQED1XWL"
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+const database = getDatabase(app);
 
-export { db };
+export { database, ref, get, child };
